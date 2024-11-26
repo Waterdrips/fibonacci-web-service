89e120031bafe81a159b8323ecae763d993356c5
Alistair Hey <alistair@heyal.co.uk>
2024-11-26 10:54:19 +0000

    Fix tests after playing around with catching errors in calculation
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

--- web-service/fibonacci_test.go
+++ web-service/fibonacci_test.go
@@ -1,31 +1,27 @@
 package main
 
 import (
-	"errors"
 	"reflect"
 	"testing"
 )
 
 func TestFibonacciSequence(t *testing.T) {
 	tests := []struct {
-		name        string
-		input       int
-		expected    []int
-		expectedErr error
+		name     string
+		input    int
+		expected []int
 	}{
-		{"negative number", -1, []int{}, nil},
-		{"zero", 0, []int{}, nil},
-		{"one", 1, []int{0}, nil},
-		{"two", 2, []int{0, 1}, nil},
-		{"five", 5, []int{0, 1, 1, 2, 3}, nil},
-		{"ten", 10, []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}, nil},
-		{"massive", 9999999999999999, []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}, errors.New("array index out of bounds")},
+		{"negative number", -1, []int{}},
+		{"zero", 0, []int{}},
+		{"one", 1, []int{0}},
+		{"two", 2, []int{0, 1}},
+		{"five", 5, []int{0, 1, 1, 2, 3}},
+		{"ten", 10, []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34}},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			result, err := fibonacciSequence(tt.input)
-
+			result := fibonacciSequence(tt.input)
 			if !reflect.DeepEqual(result, tt.expected) {
 				t.Errorf("fibonacciSequence(%d) = %v; want %v", tt.input, result, tt.expected)
 			}
